228b7c9dad5d1c36017281f7c35db64d5ec15ae4
BalquintaBelen <balquintabelen@gmail.com>
2024-03-21 11:41:47 -0300

    Hice cambios

--- Ej1p1.py
+++
@@ -1,9 +0,0 @@
-'''1. Desarrolla un programa que solicite al usuario que ingrese su edad y luego calcule y
-muestre cuántos años le faltan para alcanzar los 100 años.'''
-
-max = 100
-edad = int (input("Ingrese su edad: "))
-
-res = max - edad
-type(res)
-print(f"le faltan {res} años para llegar a los {max} años")
